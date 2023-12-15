c6ec4db4bc847eaacb6f303d76b41ea516cb1497
Kirill_Hl <151836888+KirilHl@users.noreply.github.com>
2023-12-15 16:34:05 +0700

    Update 15!12!2023.py

--- Unic_cods/15!12!2023.py
+++ Unic_cods/15!12!2023.py
@@ -1,1 +1,19 @@
+class Person: 
 
+    def init(self, name):                      #Конструктор класса          
+        self.name = name  # имя человека       #Атрибут
+        self.age = 1  # возраст человека
+tom = Person()
+tom.age = 31
+print(tom.age)                                 #Выыод информации из класса
+
+#другой код
+
+while True:
+    class Person:
+        def say(self, message):
+            print(message)
+
+
+    tom = Person()
+    tom.say(input())  # Hello work
